1a4f8b089a3be4091fe5a063a30a5eeaacca1ed1
Mega Gorilla <hahahadesu@gmail.com>
2023-09-17 23:54:49 +0900

    jsonを追加が可能に

--- API.py
+++ API.py
@@ -1,14 +1,16 @@
 #API.py
 from module.GPT_request import GPT_request
+from module.rich_desgin import error
 from rich import print
 from fastapi import FastAPI
 from pydantic import BaseModel
-import os
+import os,json,shutil,re
 import openai
 import asyncio
 import time
 
 openai_key = os.getenv("OPENAI_API_KEY")
+prompt_folder_path = "data"
 app = FastAPI()
 chank_list = []
 result_list = []
@@ -22,18 +24,60 @@ async def read_job_status(job_name: str):
 async def read_job_list():
     return job_manager.get_job_list()
 
+# 
+def get_file_list():
+    all_files = os.listdir("data")
+    json_files = [f for f in all_files if f.endswith('.json')]
+    return json_files
+
+# Jsonデータを作成する
+async def Create_or_add_json_data(title,prompt_text=None,settings=None,history=None):
+    json_file_list = get_file_list()
+    json_file_name = title + ".json"
+    json_file_path = os.path.join(prompt_folder_path,json_file_name)
+    
+    if json_file_list not in json_file_name:
+        #jsonファイルが存在しない場合新規作成する。
+        tempfilepath=os.path.join(prompt_folder_path,"template.json")
+        if os.path.exists(tempfilepath):
+            error("template.json is Not Found.","[template.json] file not found in the [data] folder.")
+            exit(1)
+        shutil.copy(tempfilepath,json_file_path)
+
+    #jsonファイルを読み込む
+    with open(json_file_path, "r", encoding='utf-8') as json_file:
+        json_data = json.load(json_file)
+
+    #データの書き込み
+    if prompt_text is not None:
+        json_data['text']=prompt_text
+        #variables 設定
+        placeholder_dict = {}
+        placeholders = re.findall(r'{(.*?)}', prompt_text)
+        for placeholder in placeholders:
+            placeholder_dict[placeholder] = ""
+        json_data['variables'] = placeholder_dict
+
+    if settings is not None:
+        for key, value in settings.items():
+            json_data['setting'][key]= value
+    
+    if history is not None:
+        json_data['history'].append(history)
+    
+    with open(json_file_path, "w", encoding='utf-8') as json_file:
+        json.dump(json_data, json_file, indent=4)
+
 #新しいasyncioタスクを作成するプロセス
 async def create_tasks(task_queue):
     #新しいタスクを作成
     if task_queue.qsize()!=0:
         tasks= await task_queue.get()
         if asyncio.iscoroutine(tasks[1]):
-            newjob = asyncio.create_task(tasks[1])
-            job_manager.add_job(tasks[0],newjob)
+            asyncio.create_task(tasks[1])
         else:
             for task in tasks:
-                newjob = asyncio.create_task(task[1])
-                job_manager.add_job(task[0],newjob)
+                asyncio.create_task(task[1])
 
 async def Create_StreamGPT_task(task_queue,result_queue,producer_id,openai_key,prompt,temp=1,tokens_max=2000,model_name='gpt-4',max_retries=3,debug=False):
     gpt_instance = GPT_request()
@@ -50,17 +94,11 @@ async def Create_StreamGPT_task(task_queue,result_queue,producer_id,openai_key,p
 
 # result_queueを監視し、新しい関数が入力されたら処理するプロセス
 async def handle_results(result_queue,task_queue):
-    speech_str=''
-    last_speech_time=time.time()
 
     while True:
         result = await result_queue.get()
         #print(f"Received result: {result}")
         print(result["message"],end=None)
-        
-        if result["ID"] == 'speech':
-            if time.time()-last_speech_time > job_manager.auto_speech_delay: #一定時間経過した場合の読み上げ
-                await Create_StreamGPT_task(task_queue,result_queue,'mirai_talk',openai_key,)
 
         await asyncio.sleep(0.1)
 
@@ -69,24 +107,12 @@ async def main():
     task_queue = asyncio.Queue(maxsize=10)
     result_queue = asyncio.Queue(maxsize=100)
 
-    #azure setup
-    handler = SpeechHandler(queue=result_queue,
-                            producer_id="speech",
-                            speech_key=speech_key, 
-                            speech_region=speech_region,
-                            TimeoutMs='1000',
-                            debug=False)
     speech_task = None
 
     # Start a task to handle results
-    process_result_task=asyncio.create_task(handle_results(result_queue,task_queue))
-    job_manager.add_job("process_result_task",process_result_task)
+    asyncio.create_task(handle_results(result_queue,task_queue))
 
     while True:
-
-        if speech_task is None or speech_task.done():
-            speech_task=asyncio.create_task(handler.from_mic())
-            job_manager.add_job("speech_task", speech_task)
         
         await create_tasks(task_queue)
             
