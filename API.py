3707cd0f02b1c3908c684ec1c76e595a5a6325a1
Mega Gorilla <hahahadesu@gmail.com>
2023-09-18 14:24:09 +0900

    GPT問い合わせ可能に

--- API.py
+++ API.py
@@ -2,19 +2,16 @@
 from module.GPT_request import GPT_request
 from module.rich_desgin import error
 from rich import print
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from pydantic import BaseModel, Field
-from typing import Optional, Dict, Union,Any
+from typing import Optional, Dict, List,Any
 import os,json,shutil,re
-import openai
 import asyncio
 import time
 
 openai_key = os.getenv("OPENAI_API_KEY")
 prompt_folder_path = "data"
 app = FastAPI()
-chank_list = []
-result_list = []
 
 class Settings(BaseModel):
     model: str
@@ -25,35 +22,91 @@ class Settings(BaseModel):
     frequency_penalty: float
     #logit_bias: Optional[Dict[int, Any]] = None  # Noneかfloatの辞書
 
+class TextItem(BaseModel):
+    system: Optional[str] = None
+    user: Optional[str] = None
+
 #item class
 class Prompts(BaseModel):
     title: str
-    prompt_text: Optional[str] = Field(None, alias="promptText")
+    description: str
+    text: List[TextItem]
     setting: Settings
 
 class Test(BaseModel):
     title: str
     prompt_text: Optional[str] = None
 
-#Get Job Status
-@app.post("/prompts/")
-async def add_new_prompt(prompt: Prompts):
-    await Create_or_add_json_data(prompt.title,prompt.prompt_text,prompt.setting)
+class variables_dict(BaseModel):
+    variables: dict = Field(default={})
 
-#Get Job Status
+class global_values:
+    prompt_list = []
+    stream_queue= asyncio.Queue()
+
+@app.post("/prompts-post/new_prompt", tags=["Prompts"])
+async def add_new_prompt(prompt: Prompts):
+    await Create_or_add_json_data(prompt.title,prompt.description,prompt.text,prompt.setting)
+
+@app.get("/prompts-get/list", tags=["Prompts"])
+async def get_prompt_list():
+    result = await get_prompts_list()
+    global_values.prompt_list = result
+    return result
+
+@app.get("/prompts-get/names", tags=["Prompts"])
+async def get_prompt_name():
+    result = await get_prompts_list()
+    global_values.prompt_list = result
+    title_list = [d["title"] for d in result]
+    return title_list
+
+@app.get("/prompts-get/history/{prompt_name}", tags=["Prompts"])
+async def get_history(prompt_name: str):
+    result = await get_history(prompt_name)
+    return result
+
+@app.post("/requst/openai/{prompt_name}", tags=["OpenAI"])
+async def OpenAI_request(prompt_name: str, item: variables_dict):
+    responce = await GPT_request_API(prompt_name, item.variables)
+    return responce
+
+#Test
 @app.post("/tests/")
 async def Test_item(test: Test):
     print(test.title)
     print(test.prompt_text)
 
-# 
+# jsonデータをリストで取得する
 def get_file_list():
     all_files = os.listdir("data")
     json_files = [f for f in all_files if f.endswith('.json')]
     return json_files
 
-# Jsonデータを作成する
-async def Create_or_add_json_data(title,prompt_text=None,settings=None,history=None):
+#プロンプトリストの取得
+async def get_prompts_list():
+    json_file_list = get_file_list()
+    result = []
+
+    for json_file in json_file_list:
+        with open(f"data/{json_file}", 'r', encoding='utf-8') as f:
+            data = json.load(f)
+            if 'history' in data:
+                del data['history']
+            result.append(data)
+    return result
+
+#プロンプト履歴の取得
+async def get_history(name):
+    result = None
+    with open(f"data/{name}.json", 'r', encoding='utf-8') as f:
+            data = json.load(f)
+            if 'history' in data:
+                result = data['history']
+    return result
+
+# Jsonデータを作成or編集する
+async def Create_or_add_json_data(title,description=None,prompt_text=None,settings=None,history=None):
     json_file_list = get_file_list()
     json_file_name = title + ".json"
     json_file_path = os.path.join(prompt_folder_path,json_file_name)
@@ -71,6 +124,9 @@ async def Create_or_add_json_data(title,prompt_text=None,settings=None,history=N
 
     #データの書き込み
     json_data['title']=title
+    if description is not None:
+        json_data['description']=description
+
     if prompt_text is not None:
         json_data['text']=prompt_text
         #variables 設定
@@ -91,27 +147,38 @@ async def Create_or_add_json_data(title,prompt_text=None,settings=None,history=N
     with open(json_file_path, "w", encoding='utf-8') as json_file:
         json.dump(json_data, json_file, indent=4)
 
-# result_queueを監視し、新しい関数が入力されたら処理するプロセス
-async def handle_results(result_queue,task_queue):
+#GPTに問い合わせ実施
+async def GPT_request_API(name,values):
+    prompt_list = global_values.prompt_list
+    filtered_list = [item for item in prompt_list if name.lower() == item['title'].lower()]
+    if len(filtered_list) == 0:
+        prompt_list=await get_prompts_list()
+        global_values.prompt_list = prompt_list
+        filtered_list = [item for item in prompt_list if name.lower() == item['title'].lower()]
+    if len(filtered_list) == 0:
+        return None
+    filtered_list = filtered_list[0]
 
-    while True:
-        result = await result_queue.get()
-        #print(f"Received result: {result}")
-        print(result["message"],end=None)
-
-        await asyncio.sleep(0.1)
-
+    text = filtered_list['text']
+    for item in text:
+        for key, value in item.items():
+            item[key] = value.format(**values)
+    response = await GPT_request().GPT_request(filtered_list['title'],
+                              openai_key,
+                              text,
+                              filtered_list['setting']['temperature'],
+                              filtered_list['setting']['max_tokens'],
+                              filtered_list['setting']['model'])
+    response['variables']= values
+    response['prompt']= text
+    await Create_or_add_json_data(name,history=response)
+    return response["choices"][0]["message"]["content"]
 
 async def main():
-    setting= {
-        "model": "testmodel",
-        "temperature": 0,
-        "top_p": 0,
-        "max_tokens": 0,
-        "presence_penalty": 0,
-        "frequency_penalty": 0
-    }
-    await Create_or_add_json_data("test_title","tsetPrompt",setting)
+    #global_values.prompt_list = await get_prompts_list()
+    process_time=time.time()
+    await GPT_request_API("template",{"things":"weather"})
+    print({(time.time())-process_time})
 
 if __name__ == "__main__":
     speech_key = os.getenv("AZURE_API_KEY")
