cafdef24962029e334595911e4b27c6fadfa9a11
Mega-Gorilla <hahahadesu@gmail.com>
2023-10-23 02:03:00 +0900

    問い合わせ機能の追加

--- GUI.py
+++ GUI.py
@@ -6,22 +6,41 @@ import ast
 import json
 
 LLM_model_name=["gpt-3.5-turbo", "gpt-4"]
+BASE_URL = "http://127.0.0.1:8000"
 
 def fetch_data_from_api():
-    response = requests.get("http://127.0.0.1:8000/prompts-get/get_prompt_metadata")
+    response = requests.get(f"{BASE_URL}/prompts-get/get_prompt_metadata")
     response.raise_for_status()
     return response.json()
 
 def edit_prompt_from_api(send_data):
     json_data= json.dumps(send_data, indent=4)
-    response = requests.post("http://127.0.0.1:8000/prompts-post/add_edit_prompt", json_data)
+    response = requests.post(f"{BASE_URL}/prompts-post/add_edit_prompt", json_data)
     response=response.json()
     if response!=None:
-        error = st.warning(f"Save Error.\nReason: {response}")
+        error = st.warning(f"Error: {response.json()}")
     else:
         success = st.success(f"Data was saved.")
         time.sleep(1)
         success.empty()
+    return response
+
+def submit_openai(prompt_name,prompt,variables):
+    json_data= {
+        "user_assistant_prompt": prompt,
+            "variables": ast.literal_eval(variables)
+            }
+    json_data= json.dumps(json_data, indent=4)
+    response = requests.post(f"{BASE_URL}/request/openai-post/{prompt_name}?stream=false", json_data)
+    
+    if response.status_code!=200:
+        error = st.warning(f"Error: {response.json()}")
+        response = None
+    else:
+        success = st.success(f"Submittied!!")
+        time.sleep(1)
+        success.empty()
+        response=response.json()
     return response
 
 def show_sidebar_buttons(data):
@@ -53,10 +72,10 @@ def show_sidebar_buttons(data):
                 "variables":{}
                     }
     
-    st.sidebar.markdown("")
+    st.sidebar.markdown("Select Prompt:")
     apply_css()
     for item in data:
-        button_label = f"**[{item['title']}]**\n{item['description']}"
+        button_label = f"**[{item['title']}]**\n\n{item['description']}"
         if st.sidebar.button(button_label, key=item['title'],use_container_width=True):
             st.session_state.selected_item = item
 
@@ -64,18 +83,19 @@ def display_selected_item_details():
     title = st.session_state.selected_item['title']
     dscription = st.session_state.selected_item['description']
     setting_dict = st.session_state.selected_item['setting']
-    test_dict = st.session_state.selected_item['text']
+    text_dict = st.session_state.selected_item['text']
     variables_dict = st.session_state.selected_item['variables']
 
     if title == "":
         title = st.text_input('Title', 'Prompt Name')
         dscription = st.text_input('Dscription', 'Prompt dscription')
-        st.markdown("* * *")
+        st.markdown("* * *\n**Settings:**")
     else:
         st.markdown(f"""
         ## <u>{title}</u>
         **{dscription}**
         """, unsafe_allow_html=True)
+        st.markdown("* * *\n**Settings:**")
     
     #Settings
     setting_model = st.selectbox(f"Model: {setting_dict['model']}",LLM_model_name,LLM_model_name.index(setting_dict['model']))
@@ -84,23 +104,33 @@ def display_selected_item_details():
     setting_topP = st.slider(f"Top P: {setting_dict['top_p']}",0.0,1.0,float(setting_dict['top_p']),0.01)
     setting_frequency = st.slider(f"Frequency penalty: {setting_dict['frequency_penalty']}",0.0,2.0,float(setting_dict['top_p']),0.01)
     setting_penalty = st.slider(f"Presence Penalty: {setting_dict['presence_penalty']}",0.0,2.0,float(setting_dict['presence_penalty']),0.01)
-    st.markdown("* * *")
+    st.markdown("* * *\n**Prompt:**")
 
-    #Display Prompts
-    prompt_data = display_prompt_text(test_dict)
-    st.markdown("* * *")
+    #Display Prompt
+
+    prompt_data = display_prompt_text(text_dict)
+
+    st.markdown("* * *\n**Variable**")
 
     variables_dict = st.text_area(label='Variables(未実装)',value=variables_dict)
 
-    if st.button('Save'):
+    #ボタン配置
+    save_button_clicked = st.button('Save',use_container_width=True)
+    #ボタン配置
+    submit_buttion_clicked = st.button('Submit',use_container_width=True,type="primary")
+    if save_button_clicked:
         setting_dict = {'model': setting_model, 'temperature': setting_temp, 'top_p': setting_topP, 'max_tokens': setting_length, 'presence_penalty': setting_penalty, 'frequency_penalty': setting_frequency}
         submit_data = {"title":title , "description":dscription} | {'texts':prompt_data} | {'setting':setting_dict}
-        
-        response = edit_prompt_from_api(submit_data)
+        edit_prompt_from_api(submit_data)
+    if submit_buttion_clicked:
+        result = submit_openai(title,[],variables_dict)
+        if result !=None:
+            st.text_area(label="", value=result,height=result.count('\n')*50)
 
-def display_prompt_text(test_dict):
+def display_prompt_text(text_dict):
     text_box_value_dict = {}
-    for key, value in test_dict.items():
+
+    for key, value in text_dict.items():
         text_box_value = st.text_area(label=key, value=value,height=value.count('\n')*35)
         text_box_value_dict[key] = text_box_value
     return text_box_value_dict
