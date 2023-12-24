e6c7dead675e0292b01d2c6ce9931b003c67dc35
Mega-Gorilla <hahahadesu@gmail.com>
2023-12-24 17:14:16 +0900

    一部修正

--- Start.ps1
+++ Start.ps1
@@ -2,7 +2,8 @@
 Start-Process powershell -ArgumentList "-NoExit","-Command & conda activate AI_Tuber; streamlit run .\GUI.py"
 
 # デフォルトのブラウザでURLを開く
-Start-Process "http://127.0.0.1:8000/docs"
+# Start-Process "http://127.0.0.1:8000/docs"
 # api.pyを実行
+conda activate AI_Tuber
 python .\api.py
 Read-Host -Prompt "Press Enter to exit"
